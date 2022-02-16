b96aa72fa4969d281e1ae7d38c94831df94c1f89
BrunoImai <47978966+BrunoImai@users.noreply.github.com>
2022-02-16 15:35:54 -0300

    Minor corrections

--- Plantinhas/GameViewController.swift
+++ Plantinhas/GameViewController.swift
@@ -85,10 +85,6 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-
         }
 
     }
